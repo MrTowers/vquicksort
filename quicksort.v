38ff00f722df3651e66321c96d4002c79025a9ff
MrTowers <dawmc52@gmail.com>
2023-06-08 08:01:35 +0200

    i need to think

--- quicksort.v
+++ quicksort.v
@@ -1,5 +1,22 @@
 module quicksort
 
 pub fn sort[T](mut array []T) {
-	
+	if array.len <= 1 {
+		return
+	}
+
+	pivot := array.len / 2
+	pivotval = array[pivot]
+	mut low := 0
+	mut high := array.len - 1
+
+	for {
+		if array[low] >= pivotval {
+			swap(array)
+		}
+	}
+}
+
+fn swap[T](mut array []T, id1 usize, id2 usize) {
+	array[id1], array[id2] = array[id2], array[id1]
 }
