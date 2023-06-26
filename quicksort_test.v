8e85b3dc2fdd0732182ffa22e59764766eeb57f3
MrTowers <dawmc52@gmail.com>
2023-06-27 01:03:21 +0200

    cleanup

--- quicksort_test.v
+++ quicksort_test.v
@@ -6,9 +6,4 @@ fn test_quicksort() {
 	mut arr := [6, 5, 2, 3]
 	in_place(mut arr)
 	assert arr == [2, 3, 5, 6]
-
-	arr.clear()
-	for i in 0 .. 5000000 {
-		arr << rand.int_in_range(0, 100) or { panic(err) }
-	}
 }
