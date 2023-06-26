cf8b9766f6b9943ddd187713549e61cc81884e6a
MrTowers <dawmc52@gmail.com>
2023-06-27 00:39:36 +0200

    v0.1.0

--- quicksort.v
+++ quicksort.v
@@ -1,22 +1,56 @@
 module quicksort
 
-pub fn sort[T](mut array []T) {
-	if array.len <= 1 {
+fn partition[T](mut arr []T, start T, end T) T {
+	pivot := arr[int(start)]
+
+	mut count := 0
+	for i := start + 1; i <= end; i++ {
+		if arr[i] <= pivot {
+			count++
+		}
+	}
+
+	pivot_index := start + count
+	swap(mut arr, pivot_index, start)
+
+	mut i := int(start)
+	mut j := int(end)
+
+	for i < pivot_index && j > pivot_index {
+		for arr[i] <= pivot {
+			i++
+		}
+
+		for arr[j] > pivot {
+			j--
+		}
+
+		if i < pivot_index && j > pivot_index {
+			swap(mut arr, i, j)
+			i++
+			j--
+		}
+	}
+
+	return pivot_index
+}
+
+fn sort[T](mut arr []T, start T, end T) {
+	if start >= end {
 		return
 	}
 
-	pivot := array.len / 2
-	pivotval = array[pivot]
-	mut low := 0
-	mut high := array.len - 1
+	p := partition(mut arr, start, end)
 
-	for {
-		if array[low] >= pivotval {
-			swap(array)
-		}
-	}
+	sort(mut arr, start, p - 1)
+	sort(mut arr, p + 1, end)
 }
 
-fn swap[T](mut array []T, id1 usize, id2 usize) {
-	array[id1], array[id2] = array[id2], array[id1]
+fn swap[T](mut arr []T, idx T, idy T) {
+	arr[int(idx)], arr[int(idy)] = arr[int(idy)], arr[int(idx)]
+}
+
+//sorts an array in place without allocating new array
+pub fn in_place[T](mut arr []T) {
+	sort(mut arr, 0, arr.len - 1)
 }
--- quicksort_test.v
+++ quicksort_test.v
@@ -1,7 +1,14 @@
 module quicksort
 
+import rand
+
 fn test_quicksort() {
 	mut arr := [6, 5, 2, 3]
-	sort(mut arr)
+	in_place(mut arr)
 	assert arr == [2, 3, 5, 6]
+
+	arr.clear()
+	for i in 0 .. 5000000 {
+		arr << rand.int_in_range(0, 100) or { panic(err) }
+	}
 }
